6bcf10a348e0be73273e68f7acf356118616b582
aptyp222 <aptyp222@me.com>
2019-04-26 00:20:56 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ sudo apt-get install mc -y
 sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
 sudo npm install -g homebridge-people 
 sudo npm install -g homebridge-raspberrypi-temperature 
-sudo npm install -g homebridge-ds18b20
-sudo npm install -g homebridge-mi-aqara && sudo npm install -g miio 
-sudo npm install -g --unsafe-perm homebridge-gpio-garagedoor 
-sudo npm install -g homebridge-yeelight-wifi 
+//sudo npm install -g homebridge-ds18b20
+//sudo npm install -g homebridge-mi-aqara && sudo npm install -g miio 
+//sudo npm install -g --unsafe-perm homebridge-gpio-garagedoor 
+//sudo npm install -g homebridge-yeelight-wifi 
 sudo npm install -g homebridge-camera-ffmpeg
  
 sudo cp homebridge.service /etc/systemd/system/
@@ -23,24 +23,24 @@ sudo systemctl daemon-reload
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
 
-cd /tmp
-wget https://lion.drogon.net/wiringpi-2.46-1.deb
-sudo dpkg -i wiringpi-2.46-1.deb
-sudo usermod -G gpio pi
-cd ..
-sudo npm install -g homebridge-gpio-device --unsafe-perm
+//cd /tmp
+//wget https://lion.drogon.net/wiringpi-2.46-1.deb
+//sudo dpkg -i wiringpi-2.46-1.deb
+//sudo usermod -G gpio pi
+//cd ..
+//sudo npm install -g homebridge-gpio-device --unsafe-perm
 
 
 
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.58.tar.gz
-tar zxvf bcm2835-1.58.tar.gz
-cd bcm2835-1.58
-./configure && make && sudo make check && sudo make install
-cd ..
-sudo npm install -g --unsafe-perm homebridge homebridge-pi-thermostat
+//wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.58.tar.gz
+//tar zxvf bcm2835-1.58.tar.gz
+//cd bcm2835-1.58
+//./configure && make && sudo make check && sudo make install
+//cd ..
+//sudo npm install -g --unsafe-perm homebridge homebridge-pi-thermostat
 
 sudo apt-get install samba samba-common-bin -y
-sudo modprobe w1-gpio && sudo modprobe w1_therm && ls -l /sys/bus/w1/devices/
+//sudo modprobe w1-gpio && sudo modprobe w1_therm && ls -l /sys/bus/w1/devices/
 
 
 homebridge -I -U
